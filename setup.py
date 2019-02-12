6dc40d847e8d48c37aa2f6b73ee2412f2c8a735c
Eric Pellegrini <pellegrini@ill.fr>
2019-02-12 10:30:29 +0100

    added pypi packaging info

--- setup.py
+++ setup.py
@@ -8,6 +8,14 @@ except ImportError:
 pkginfo = {}
 exec(open("src/__pkginfo__.py","r").read(),{},pkginfo)
 
+# Add additional information to pkginfo
+pkginfo["__classifiers__"] = ["Programming Language :: Python :: 3","License :: OSI Approved :: MIT License","Operating System :: OS Independent"]
+
+pkginfo["__long_description_content_type__"] ="text/markdown"
+
+with open("README.md","r") as f:
+    pkginfo["__long_description__"] = f.read()
+
 def is_package(path):
     return (os.path.isdir(path) and os.path.isfile(os.path.join(path, '__init__.py')))
 
@@ -26,18 +34,20 @@ def find_packages(path, base=None, exclude=None):
 
 packages = find_packages(path="src",base="hdfviewer")
 
-setup(name             = "hdfviewer",
-      version          = pkginfo["__version__"],
-      description      = pkginfo["__description__"],
-      long_description = pkginfo["__long_description__"],
-      author           = pkginfo["__author__"],
-      author_email     = pkginfo["__author_email__"],
-      maintainer       = pkginfo["__maintainer__"],
-      maintainer_email = pkginfo["__maintainer_email__"],
-      url              = pkginfo["__url__"],
-      license          = pkginfo["__license__"],
-      packages         = packages,
-      package_dir      = {"hdfviewer" : "src"},
-      platforms        = ['Unix','Windows'],
-      install_requires = ["numpy","matplotlib","h5py","jupyterlab","ipywidgets","ipympl"]
+setup(name                          = "hdfviewer",
+      version                       = pkginfo["__version__"],
+      description                   = pkginfo["__description__"],
+      long_description              = pkginfo["__long_description__"],
+      long_description_content_type = pkginfo["__long_description_content_type__"],
+      author                        = pkginfo["__author__"],
+      author_email                  = pkginfo["__author_email__"],
+      maintainer                    = pkginfo["__maintainer__"],
+      maintainer_email              = pkginfo["__maintainer_email__"],
+      url                           = pkginfo["__url__"],
+      license                       = pkginfo["__license__"],
+      classifiers                   = pkginfo["__classifiers__"],
+      packages                      = packages,
+      package_dir                   = {"hdfviewer" : "src"},
+      platforms                     = ['Unix','Windows'],
+      install_requires              = ["numpy","matplotlib","h5py","jupyterlab","ipywidgets","ipympl"]
 )
