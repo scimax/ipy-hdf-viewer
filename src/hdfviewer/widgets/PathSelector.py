39e94c4fd280e6c5f340747e85459a4f8962d6f9
Eric Pellegrini <pellegrini@.ill.fr>
2019-02-19 16:15:29 +0100

    refactored PathSelector class

--- src/hdfviewer/widgets/PathSelector.py
+++ src/hdfviewer/widgets/PathSelector.py
@@ -8,6 +8,7 @@ import ipywidgets as widgets
 
 from IPython.display import display
 
+
 class PathSelector(object):
     """This class allows to create a file browser in the context of **Jupyter Lab**
 
@@ -21,100 +22,118 @@ class PathSelector(object):
     :type extensions: list[int]
     """
 
-    def __init__(self,startDir=None,selectFile=True,extensions=None):        
+    def __init__(self, startingPath=None, selectFile=True, extensions=None):
 
-        self._file  = None 
         self._selectFile = selectFile
-        self._startDir = startDir if startDir else os.environ["HOME"]
-        self._select = widgets.SelectMultiple(options=['init'],value=(),rows=10,description='') 
-        self._widget = widgets.Accordion(children=[self._select])        
-        self._extensions = extensions if extensions else []
 
-        # Start closed (showing path only)
+        self._select = widgets.SelectMultiple(
+            options=[], value=(), rows=10, description='')
+        self._widget = widgets.Accordion(children=[self._select])
+        self._extensions = extensions if extensions else []
         self._widget.selected_index = None
-        self.update(self._startDir)
-        self._select.observe(self._onUpdate,'value')
+
+        self.update(startingPath)
+
+        self._select.observe(self._onUpdate, 'value')
 
     @property
-    def file(self):
+    def path(self):
         """Return the path selected.
 
         :return: the selected path
         :rtype: str
         """
 
-        return self._file
+        return self._path
+
+    @path.setter
+    def path(self, path):
+
+        self._path = path
 
     @property
     def widget(self):
         """Getter for the file browser widget
 
         :return: the file browser widget
-        :rtype: `ipywidgets.Accordion <https://ipywidgets.readthedocs.io/en/stable/examples/Widget%20List.html#Accordion-and-Tab-use-selected_index,-not-value>`_
+        # Accordion-and-Tab-use-selected_index,-not-value>`_
+        :rtype: `ipywidgets.Accordion <https://ipywidgets.readthedocs.io/en/stable/examples/Widget%20List.html
         """
 
         return self._widget
 
-    def _onUpdate(self,change):
+    def _onUpdate(self, change):
         """A callable that is called when a new entry of the file browser is clicked
 
-        See `here <https://ipywidgets.readthedocs.io/en/stable/examples/Widget%20Events.html#Traitlet-events>`_ for more information
+        # Traitlet-events>`_ for more information
+        See `here <https://ipywidgets.readthedocs.io/en/stable/examples/Widget%20Events.html
         """
 
         if len(change['new']) > 0:
-            self.update(change['new'][0])
+            self.update(os.path.join(self._currentDir, change['new'][0]))
 
-    def update(self,item):
-        """Update the file browser widget with a new entry (file or directory name)
+    def update(self, path):
+        """Update the file browser widget with a new file or directory basename
 
         This will:
 
         1. update the name of the current selection on top of the file browser widget
         2. update the directory contents subwidget in case where the new entry is a directory
 
-        :param entry: the filename or directory name to update the file browser with
-        :type entry: str
+        :param basename: the new file or directory basename
+        :type basename: str
         """
 
-        path = os.path.abspath(os.path.join(self._startDir,item))
+        if path is None:
+            path = os.getcwd()
 
-        if os.path.isfile(path):
-            if self._selectFile:
-                self._widget.set_title(0,path)  
-                self._file = path
+        # Make the path absolute
+        path = os.path.abspath(path)
+
+        if not os.path.exists(path):
+            return
+
+        self._path = None
+        # Case of a file browser
+        if self._selectFile:
+            if os.path.isfile(path):
+                self._path = path
                 self._widget.selected_index = None
+                self._currentDir = os.path.dirname(path)
+                self._widget.set_title(0, path)
+            elif os.path.isdir(path):
+                self._currentDir = path
             else:
-                self._select.value = ()
-        else: 
-            self._file = None 
-            self._startDir  = path
+                return
+        else:
+            self._currentDir = path
+            self._widget.set_title(0, path)
 
-            # Build list of files and dirs
-            keys = ['[..]']; 
-            for item in os.listdir(path):
-                if item[0] == '.':
-                    continue
-                elif os.path.isdir(os.path.join(path,item)):
-                    keys.append('['+item+']'); 
-                else:
+        # Build list of files and dirs
+        keys = ['[..]']
+        for item in os.listdir(self._currentDir):
+            if item[0] == '.':
+                continue
+            elif os.path.isdir(os.path.join(path, item)):
+                keys.append('['+item+']')
+            else:
+                if self._selectFile:
                     if self._extensions:
                         ext = os.path.splitext(item)[-1]
                         if ext in self._extensions:
-                            keys.append(item); 
+                            keys.append(item)
                     else:
-                        keys.append(item); 
+                        keys.append(item)
 
-            # Sort and create list of output values
-            keys.sort(key=str.lower)
-            vals = []
-            for k in keys:
-                if k[0] == '[':
-                    vals.append(k[1:-1]) # strip off brackets
-                else:
-                    vals.append(k)
+        # Sort and create list of output values
+        keys.sort(key=str.lower)
+        vals = []
+        for k in keys:
+            if k[0] == '[':
+                vals.append(k[1:-1])  # strip off brackets
+            else:
+                vals.append(k)
 
-            # Update widget
-            self._widget.set_title(0,path)  
-            self._select.options = list(zip(keys,vals)) 
-            with self._select.hold_trait_notifications():
-                self._select.value = ()
+        self._select.options = list(zip(keys, vals))
+        with self._select.hold_trait_notifications():
+            self._select.value = ()
